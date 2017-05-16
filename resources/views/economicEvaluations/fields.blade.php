ce84a4e1ebda4330f6eb05afefc9a9d5e08bd212
jcmontejo <jncrlsmontejo@gmail.com>
2017-05-16 16:34:07 -0500

    Se modificaron campos de entrada de evaluación economica

--- resources/views/economicEvaluations/fields.blade.php
+++ resources/views/economicEvaluations/fields.blade.php
@@ -34,50 +34,60 @@
     {!! Form::text('address', null, ['class' => 'form-control']) !!}
 </div>
 
+<div class="form-group col-sm-12 col-lg-12">
+    <p style="text-align: center;"> 
+        ANALISIS DE DATOS DE CRÉDITO DIARIO
+    </p>
+    <p style="text-align: center;"> 
+        <strong>DATOS SUJETOS A VERIFICACIÓN</strong>
+    </p>
+</div>
+
 <!--- Sales Field --->
 <div class="form-group col-sm-6 col-lg-4">
-    {!! Form::label('sales', 'Sales:') !!}
+    {!! Form::label('sales', 'Ventas:') !!}
     {!! Form::text('sales', null, ['class' => 'form-control']) !!}
 </div>
 
 <!--- Buy Field --->
 <div class="form-group col-sm-6 col-lg-4">
-    {!! Form::label('buy', 'Buy:') !!}
+    {!! Form::label('buy', 'Compras:') !!}
     {!! Form::text('buy', null, ['class' => 'form-control']) !!}
 </div>
 
 <!--- Gross Profit Field --->
 <div class="form-group col-sm-6 col-lg-4">
-    {!! Form::label('gross_profit', 'Gross Profit:') !!}
+    {!! Form::label('gross_profit', 'Utilidad Bruta:') !!}
     {!! Form::text('gross_profit', null, ['class' => 'form-control']) !!}
 </div>
 
 <!--- Other Income Field --->
 <div class="form-group col-sm-6 col-lg-4">
-    {!! Form::label('other_income', 'Other Income:') !!}
+    {!! Form::label('other_income', 'Otros Ingresos:') !!}
     {!! Form::text('other_income', null, ['class' => 'form-control']) !!}
 </div>
 
 <!--- Other Expenses Field --->
 <div class="form-group col-sm-6 col-lg-4">
-    {!! Form::label('other_expenses', 'Other Expenses:') !!}
+    {!! Form::label('other_expenses', 'Otros Gastos:') !!}
     {!! Form::text('other_expenses', null, ['class' => 'form-control']) !!}
 </div>
 
 <!--- Familiar Costs Field --->
 <div class="form-group col-sm-6 col-lg-4">
-    {!! Form::label('familiar_costs', 'Familiar Costs:') !!}
+    {!! Form::label('familiar_costs', 'Gastos Familiares:') !!}
     {!! Form::text('familiar_costs', null, ['class' => 'form-control']) !!}
 </div>
 
 <!--- Montly Net Income Field --->
 <div class="form-group col-sm-6 col-lg-4">
-    {!! Form::label('montly_net_income', 'Montly Net Income:') !!}
+    {!! Form::label('montly_net_income', 'Utilidad Neta Mensual:') !!}
     {!! Form::text('montly_net_income', null, ['class' => 'form-control']) !!}
 </div>
 
 
 <!--- Submit Field --->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+    {!! Form::submit('Guardar', ['class' => 'uppercase btn btn-primary']) !!}
+    {!! Form::submit('Aplicar Formulas', ['class' => 'uppercase btn bg-navy']) !!}
 </div>
